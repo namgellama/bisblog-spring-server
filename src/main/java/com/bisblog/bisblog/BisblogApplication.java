9021cb1d94a949c029da298cb92c4b469ec1572f
Namgel <namgeltamang14@gmail.com>
2024-10-13 13:17:28 +0545

    Initial commit

---
+++ src/main/java/com/bisblog/bisblog/BisblogApplication.java
@@ -0,0 +1,13 @@
+package com.bisblog.bisblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bisblog/bisblog/BisblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisblog.bisblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
