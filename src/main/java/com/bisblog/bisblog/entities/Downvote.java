94d01ec045b2f426cb446418bb9643a1c0e240f5
Namgel <namgeltamang14@gmail.com>
2024-10-14 11:19:08 +0545

    Create downvote entity

---
+++ src/main/java/com/bisblog/bisblog/entities/Downvote.java
@@ -0,0 +1,30 @@
+package com.bisblog.bisblog.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "post_id"})})
+public class Downvote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
